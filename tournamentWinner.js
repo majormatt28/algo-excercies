b912d8ff3b17a173a498f51b642897a812e82e5c
Matthew <elijah2895@hotmail.com>
2022-03-02 18:48:30 -0500

    working on the smaller functions

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -4,18 +4,31 @@
     ["Python", "HTML"]
 ]
 
-let results1 = [0, 0, 1]
+let results1 = [0, 1, 2]
 
 const homeTeamWins = 1;
 
 function tournamentWinner(competitions, results) {
     let currWinningTeam = '';
-    let talliedScores = {[currWinningTeam]: 0};
-
+    // let talliedScores = {[currWinningTeam]: 0};
+  
     for (let i = 0; i < competitions.length; i++) {
         let result = results[i];
-        console.log("is this my console log: ",result);
+        // console.log("these are the competitions", competitions);
+        for (let j = 0; j < result.length; j++) {
+            let numWins = result[j];
+            const isTallied = talliedScores(numWins);
+        }
+        // console.log("is this my console log: ",result);
     }
+}
+
+function talliedScores(scores) {
+    return scores * 3;
+}
+
+function updateScores() {
+    
 }
 
 console.log(tournamentWinner(competitions1, results1));
