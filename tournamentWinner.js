e2a417ccc681004cdce33fb239776cf317c11871
Matthew <elijah2895@hotmail.com>
2022-03-04 13:40:36 -0500

    finished tournament winner

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -4,31 +4,53 @@
     ["Python", "HTML"]
 ]
 
-let results1 = [0, 1, 2]
+let results1 = [0, 0, 1];
 
 const homeTeamWins = 1;
 
 function tournamentWinner(competitions, results) {
+    let talliedScores = tallyScores(competitions, results);
+    return findWinner(talliedScores);
+}
+
+function findWinner(talliedScores) {
     let currWinningTeam = '';
-    // let talliedScores = {[currWinningTeam]: 0};
-  
-    for (let i = 0; i < competitions.length; i++) {
-        let result = results[i];
-        // console.log("these are the competitions", competitions);
-        for (let j = 0; j < result.length; j++) {
-            let numWins = result[j];
-            const isTallied = talliedScores(numWins);
+    let currHighestScore = 0;
+    for(let competitor in talliedScores) {
+        let scoreOfThisCompetitor = talliedScores[competitor];
+        if (scoreOfThisCompetitor > currHighestScore) {
+            currHighestScore = scoreOfThisCompetitor;
+            currWinningTeam = competitor;
         }
-        // console.log("is this my console log: ",result);
+    }
+    return currWinningTeam;
+}
+
+function tallyScores(competitions, results) {
+    let tally = {};
+    for(let i = 0; i < competitions.length; i++) {
+        let result = results[i];
+        let competition = competitions[i];
+        let compWinner = findCompWinner(competition, result);
+        let currentScore = tally[compWinner];
+        if(currentScore === undefined) {
+            currentScore = 0;
+        }
+        tally[compWinner] = currentScore + 1;
+    }
+    return tally;
+}
+
+function findCompWinner(competition,result) {
+    let homeTeam = competition[0];
+    let awayTeam = competition[1];
+
+    if (result === 1){
+        return homeTeam;
+    } else {
+        return awayTeam;
     }
 }
 
-function talliedScores(scores) {
-    return scores * 3;
-}
-
-function updateScores() {
-    
-}
 
 console.log(tournamentWinner(competitions1, results1));
