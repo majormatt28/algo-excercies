c2adc56e9a20860f7ef39c37cb2d5725d1b13d65
Matthew <elijah2895@hotmail.com>
2022-03-23 18:52:35 -0400

    refactoring

--- practice.js
+++ practice.js
@@ -1,23 +1,38 @@
 let inputA = ['{"studentId": 7530, "name": "Robert Lee", "graduating": true}','{"name": "Carlin B.", "graduating": true}','{"studentId": 6679, "name": "Jacqui Ostermann", "graduating": true}','{"studentId": 1256, "name": "Dhiraj Ostermann", "graduating": false}'];
 
-function formatRecords( studentsRecordDetails ) {
-  	return studentsRecordDetails
-  .map(function(student) {
-    console.log('Map: ', student);
-  		return JSON.parse(student);
-  })
-	.filter(function(student) {
-	    console.log('Filter: ', student.graduating);
-   		return student.graduating && student.studentId;
-  })
-  .reduce(function (accumulator, currentValue, index) {
-	    console.log('Reduce: ', accumulator);
-	    console.log('currentValue: ', currentValue);
-	    console.log("What is the index:",index);
-  		return (accumulator ? accumulator + ', ' : accumulator) + currentValue.name;
-  }, ""); 
-  return;
+// function formatRecords( studentsRecordDetails ) {
+//   	return studentsRecordDetails
+//   .map(function(student) {
+//     console.log('Map: ', student);
+//   		return JSON.parse(student);
+//   })
+// 	.filter(function(student) {
+// 	    console.log('Filter: ', student.graduating);
+//    		return student.graduating && student.studentId;
+//   })
+//   .reduce(function (accumulator, currentValue, index) {
+// 	    console.log('Reduce: ', accumulator);
+// 	    console.log('currentValue: ', currentValue);
+// 	    console.log("What is the index:",index);
+//   		return (accumulator ? accumulator + ', ' : accumulator) + currentValue.name;
+//   }, ""); 
+//   return;
+// }
+
+
+
+function formatRecords (studentsRecordDetails) {
+    filterStudents(studentsRecordDetails);
+    return studentsRecordDetails.map(student => JSON.parse(student));
 }
+
+function filterStudents (student) {
+    return student.graduating && student.studentId;
+}
+
+
+
+
 
 
 let result = formatRecords(inputA);
