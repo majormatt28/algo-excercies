3bc96f1f2ac93d3925d72c1f49b9326dc7ebf322
Matthew <elijah2895@hotmail.com>
2022-04-07 10:54:09 -0400

    worked on minimumWaitTIme

--- minimumWaitTime.js
+++ minimumWaitTime.js
@@ -1,11 +1,18 @@
 let myQueries = [3,2,1,2,6]
+// sample output: 17
 
 function minimumWaitingTime(queries) {
-    let totalWaitingTime = 0;
+    let totalWaitTime = 0;
 
     for (let i = 0; i < queries.length; i++) {
-        
+        // console.log(queries[i]);
+        let durations = queries[i];
+        let queriesRemaining = queries.length - (i + 1);
+        console.log("This is the amount of queries remaining: ",queriesRemaining);
+        totalWaitTime += durations * queriesRemaining;
+        console.log("This is the total wait time: ",totalWaitTime)
     }
+    return totalWaitTime;
 }
 
-console.log(minimumWaitingTime(myQueries));
+console.log("Final Results: ",minimumWaitingTime(myQueries));
