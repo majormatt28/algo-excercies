7707104fb6d1799e59636675c83a96a6c4d540a4
Matthew <elijah2895@hotmail.com>
2022-04-07 11:30:22 -0400

    finished minimum wait time

--- minimumWaitTime.js
+++ minimumWaitTime.js
@@ -1,16 +1,16 @@
-let myQueries = [3,2,1,2,6]
+let myQueries = [2,3,1,2,6]
 // sample output: 17
 
 function minimumWaitingTime(queries) {
     let totalWaitTime = 0;
-
+    queries.sort();
     for (let i = 0; i < queries.length; i++) {
         // console.log(queries[i]);
         let durations = queries[i];
         let queriesRemaining = queries.length - (i + 1);
         console.log("This is the amount of queries remaining: ",queriesRemaining);
         totalWaitTime += durations * queriesRemaining;
-        console.log("This is the total wait time: ",totalWaitTime)
+        console.log("This is how much this task will cost: ",totalWaitTime)
     }
     return totalWaitTime;
 }
